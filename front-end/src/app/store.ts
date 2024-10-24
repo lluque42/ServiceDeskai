7951a6896583e21fdd5667a9c2611b371128ccb1
Pedro Gomez Ruiz <pgruz11@Pedros-MacBook-Pro.local>
2024-10-24 11:19:44 +0200

    redux_tuto

---
+++ front-end/src/app/store.ts
@@ -0,0 +1,6 @@
+//Standard Redux Store setup suggested by redux-toolkit documentation
+import { configureStore } from "@reduxjs/toolkit";
+
+export const	store = configureStore({
+	reducer: {},
+});
