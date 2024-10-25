b5bf9873883145b199442d57c12eae3e3e29d0db
Pedro jesus Gomez ruiz <pgomez-r@c1r9s4.42malaga.com>
2024-10-25 12:48:12 +0200

    working before changing folder structure

--- front-end/src/components/TaskForm.js
+++ front-end/src/components/TaskForm.js
@@ -1,4 +1,4 @@
-import {useState, useEffect} from "react";
+import {useState, useEffect, useRef} from "react";
 import {useDispatch, useSelector}	from 'react-redux';
 import {useNavigate, useParams, Link} from 'react-router-dom';
 import {v4 as uuid} from 'uuid';
@@ -14,6 +14,8 @@ function	TaskForm()
 	const	navigate = useNavigate();
 	const	params = useParams();
 	const	tasks = useSelector(state => state.tasks);
+  	const	videoRef = useRef(null);
+	const	streamRef = useRef(null);
 
 	const handleKeyDown = (e) => {
 		if (e.key === 'Enter' && !e.shiftKey) {
@@ -50,33 +52,63 @@ function	TaskForm()
 		{
 			setTask(tasks.find(task => task.id === params.id));
 		}
-	}, []);
+	}, [params.id, tasks]);
+
+	useEffect(() => {
+		let video = videoRef.current;
+		navigator.mediaDevices.getUserMedia({ video: true })
+		  .then(stream => {
+			streamRef.current = stream;
+			video.srcObject = stream;
+			video.addEventListener('loadedmetadata', () => {
+				video.play();
+			})
+		  })
+		  .catch(err => {
+			console.error("Error accessing webcam: ", err);
+		  });
+		  return () => {
+			if (streamRef.current) {
+				streamRef.current.getTracks().forEach(track => track.stop());
+			}
+		  };
+	  }, []);
 
 	return (
 		<div>
 		<header>
 			<h1>Create Ticket</h1>
 		</header>
-		<form onSubmit={handleSubmit}>
+		<form onSubmit={handleSubmit} className="bg-zinc-800 max-w-sm p-4 mb-4">
+			<label htmlFor="title" className="block text-xm font-bold mb-2">Task</label>
 			<input 
 				name="title"
 				type="text"
 				placeholder="Ticket title"
 				onChange={handleChange}
 				value={task.title}
+				className="w-full p-2 rounded-md bg-zinc-600 mb-2"
 			/>
+			<label htmlFor="description" className="block text-xm font-bold mb-2">Description</label>
 			<textarea
 				name="description"
 				placeholder="Ticket description"
 				onChange={handleChange}
 				value={task.description}
 				onKeyDown={handleKeyDown}
+				className="w-full p-2 rounded-md bg-zinc-600 mb-2"
 			/>
-			<button>Save</button>
+			<label htmlFor="video" className="block text-xm font-bold mb-2">Photo</label>
+		  	<video
+				ref={videoRef}
+				className="w-full h-48 bg-black rounded-md"
+				controls
+		  	/>
+			<button className="bg-green-600 px-2 py-1 mt-4 rounded-sm">Save</button>
 		</form>
-		<Link to='/'>
-			Ticket List
-		</Link>
+			<Link to='/' className="bg-indigo-600 px-2 py-1 rounded-sm text-sm">
+				Ticket List
+			</Link>
 		</div>
 	);
 }
--- front-end/src/components/TasksList.js
+++ front-end/src/components/TasksList.js
@@ -13,7 +13,7 @@ function	TaskList()
 	return (
 		<div className="w-4/6">
 			<header className="flex justify-between item-center py-4">
-				<h1>Tickets {tasks.length}</h1>
+				<h1>Total tickets {tasks.length}</h1>
 				<Link to='create-task' className="bg-indigo-600 px-2 py-1 rounded-sm text-sm">
 					Create Ticket
 				</Link>
@@ -21,16 +21,17 @@ function	TaskList()
 			<div className="grid grid-cols-3 gap-4 ">
 			{tasks.map(task => (
 				<div key={task.id} className="bg-neutral-800 p-4 rounded-md">
-					<header className="flex justify-between">
+					<header className="flex justify-between py-2">
 						<h3>{task.title}</h3>
-						<div className="flex">
-							<button onClick={() => handleDelete(task.id)}
-								className="bg-green-500 px-2 py-1 text-xs rounded-md self-center"
-								>Delete Task</button>
-							<Link to={`/edit-task/${task.id}`}>Edit</Link>
-						</div>
 					</header>
 					<p>{task.description}</p>
+					<div className="flex gap-x-2 py-2">
+						<Link to={`/edit-task/${task.id}`}
+							className="bg-zinc-600 px-2 py-1 text-xs rounded-md">Edit</Link>
+						<button onClick={() => handleDelete(task.id)}
+							className="bg-red-500 px-2 py-1 text-xs rounded-md"
+							>Delete Task</button>
+					</div>
 				</div>
 			))}
 			</div>
